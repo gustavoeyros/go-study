6d47959c5ed4e39c28d6e9688abd6deb44013674
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-04-27 12:17:45 -0300

    feat: class 7 - methods - pt2

--- 7_metodos/main.go
+++ 7_metodos/main.go
@@ -15,13 +15,24 @@ type Categoria struct {
 	Pai  *Categoria // para utilizar uma struct dentro da própria struct, precisa utilizar o *
 }
 
+func (p Pessoa) String() string {
+	return fmt.Sprintf("Olá, meu nome é %s e eu tenho %d anos.", p.Nome, p.Idade)
+}
+
 func (c Categoria) HasParent() bool {
 	return c.Pai != nil
 }
 
+func (c *Categoria) SetPai(pai *Categoria) {
+	c.Pai = pai
+}
 func main() {
 
+	p := Pessoa{"Gustavo", "Eyros", 18, true, "000"}
+	fmt.Println(p)
+
 	cat := Categoria{Nome: "Categoria 1"}
+	cat.SetPai((&Categoria{Nome: "Pai"}))
 	if !cat.HasParent() {
 		fmt.Println("n tem pai")
 	}
